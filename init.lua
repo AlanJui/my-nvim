e6d7a6ca212ef522199310344819fb12fd66c98e
Alan Jui <AlanJui.1960@gmail.com>
2021-11-13 11:23:18 +0800

    增加快捷鍵，如：整行搬移

--- init.lua
+++ init.lua
@@ -228,3 +228,24 @@ keymap('n', 'Y', 'y$', opts)
 
 keymap('n', '\\', ':Explore<CR>', opts)
 keymap('n', '<LocalLeader>f', ':Telescope<CR>', opts)
+
+-- Move line
+keymap('n', '<S-Down>', ':m .+1<CR>', opts)
+keymap('n', '<S-Up>',   ':m .-2<CR>', opts)
+keymap('i', '<S-Down>', '<Esc>:m .+1<CR>', opts)
+keymap('i', '<S-Up>',   '<Esc>:m .-2<CR>', opts)
+keymap('v', '<S-Down>', ":move '>+1<CR>gv-gv", opts)
+keymap('v', '<S-Up>',   ":move '<-2<CR>gv-gv", opts)
+
+-- Comment
+keymap('n', '<C-_>', ':CommentToggle<CR>', { noremap = true })
+keymap('v', '<C-_>', ':CommentToggle<CR>', { noremap = true })
+
+-- Windows navigation
+keymap('n', '<C-w>-',  ':split<CR>',  opts)
+keymap('n', '<C-w>_',  ':vsplit<CR>', opts)
+keymap('n', '<C-w>|',  ':vsplit<CR>', opts)
+
+-- Window Zoom In/Out
+keymap('n', '<LocalLeader>wi', '<C-w>| <C-w>_', opts)
+keymap('n', '<LocalLeader>wo', '<C-w>=', opts)
