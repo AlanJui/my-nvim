0fcf9b646fe8c94f74a230d40195b83c6a5006ec
Alan Jui <AlanJui.1960@gmail.com>
2021-11-23 14:27:29 +0800

    修正 Gist 無法正常使用的問題

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -211,6 +211,8 @@ return require('packer').startup({
             config = vim.cmd([[
                 let g:gist_clip_command = 'xclip -selection clipboard'
                 let g:gist_open_browser_after_post = 1
+                let g:github_user = 'AlanJui'
+                let g:gist_token = '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'
             ]])
         }
 
