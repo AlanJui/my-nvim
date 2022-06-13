4cb2912d9cf00af41c663321f4e4ab1f8d406f7c
Alan Jui <alanjui.1960@gmail.com>
2022-06-13 20:56:36 +0800

    修訂「無法自 .html 檔案內容，辨識該檔案之 filetype 是否為 htmldjango 之問題」。

--- lua/settings.lua
+++ lua/settings.lua
@@ -11,3 +11,30 @@ let g:tex_flavor = "latex"
 -- autocmd BufWinLeave *.* mkview
 -- autocmd BufWinEnter *.* silent loadview
 -- ]]
+
+-- Django
+vim.cmd [[
+autocmd FileType htmldjango setlocal shiftwidth=4 tabstop=4 softtabstop=4
+autocmd FileType htmldjango inoremap {{ {{  }}<left><left><left>
+autocmd FileType htmldjango inoremap {% {%  %}<left><left><left>
+autocmd FileType htmldjango inoremap {# {#  #}<left><left><left>
+
+augroup filetypedetect
+  " removes current htmldjango detection located at $VIMRUNTIME/filetype.vim
+  autocmd! BufNewFile,BufRead *.html
+  autocmd  BufNewFile,BufRead *.html call FThtml()
+
+  func! FThtml()
+    let n = 1
+    while n < 10 && n < line("$")
+        if getline(n) =~ '{%\|{{\|{#'
+        setf htmldjango
+        return
+      endif
+      let n = n + 1
+    endwhile
+    "setf html
+    setf html
+  endfunc
+augroup END
+]]
