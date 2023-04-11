525915903dfe9c0afa87a9647f624320aa2fb9c5
居正中 <AlanJui.1960@gmail.com>
2023-04-11 22:24:22 +0800

    以 Lazy.nvim 使用之插件為基底，套用過來。

--- lua/plugins/essential.lua
+++ lua/plugins/essential.lua
@@ -1,48 +1,40 @@
 return {
-    -----------------------------------------------------------
-    -- Essential plugins
-    -----------------------------------------------------------
-    -- lua functions that many plugins use
-    "nvim-lua/plenary.nvim",
-    -- Tools to migrating init.vim to init.lua
-    "norcalli/nvim_utils",
-    -----------------------------------------------------------
-    -- Treesitter: for better syntax
-    -----------------------------------------------------------
-    -- Nvim Treesitter configurations and abstraction layer
-    {
-        "nvim-treesitter/nvim-treesitter",
-        run = ":TSUpdate",
-    },
-    -----------------------------------------------------------
-    -- Fuzzy files finder: Telescope
-    -----------------------------------------------------------
-    {
-        "nvim-telescope/telescope.nvim",
-        branch = "0.1.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-telescope/telescope-live-grep-raw.nvim",
-        },
-    },
-    -- Telescope Extensions
-    -- 'cljoly/telescope-repo.nvim',
-    "nvim-telescope/telescope-file-browser.nvim",
-    "nvim-telescope/telescope-ui-select.nvim",
-    "dhruvmanila/telescope-bookmarks.nvim",
-    "nvim-telescope/telescope-github.nvim",
-    -- Trying command palette
-    "LinArcX/telescope-command-palette.nvim",
-    {
-        "AckslD/nvim-neoclip.lua",
-        config = function()
-            require("neoclip").setup()
-        end,
-    },
-    -- dependency for better sorting performance
-    {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        run = "make",
-    },
-    "jvgrootveld/telescope-zoxide",
+	-----------------------------------------------------------
+	-- Essential plugins
+	-----------------------------------------------------------
+	-- lua functions that many plugins use
+	"nvim-lua/plenary.nvim",
+	-- Tools to migrating init.vim to init.lua
+	"norcalli/nvim_utils",
+	-----------------------------------------------------------
+	-- Fuzzy files finder: Telescope
+	-----------------------------------------------------------
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-live-grep-raw.nvim",
+		},
+	},
+	-- Telescope Extensions
+	-- 'cljoly/telescope-repo.nvim',
+	"nvim-telescope/telescope-file-browser.nvim",
+	"nvim-telescope/telescope-ui-select.nvim",
+	"dhruvmanila/telescope-bookmarks.nvim",
+	"nvim-telescope/telescope-github.nvim",
+	-- Trying command palette
+	"LinArcX/telescope-command-palette.nvim",
+	{
+		"AckslD/nvim-neoclip.lua",
+		config = function()
+			require("neoclip").setup()
+		end,
+	},
+	-- dependency for better sorting performance
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		run = "make",
+	},
+	"jvgrootveld/telescope-zoxide",
 }
